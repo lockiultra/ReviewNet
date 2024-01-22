5d5562b8354872416fe85237ee6a5b32ca18345b
lockiultra <lockiultra@gmail.com>
2024-01-22 06:37:03 +0300

    Some updates

--- model_train.py
+++ model_train.py
@@ -33,11 +33,11 @@ def get_metric_score(model: ReviewNet, metric: str, test_loader: DataLoader) -> 
             y_batch = y_batch.type(torch.int64)
             y_pred = model(X_batch)
             y_pred = torch.argmax(y_pred, dim=1)
-            metric_score = metric_fn.compute(predictions=y_pred, references=y_batch)
-            scores.append(metric_score)
+            metric_score = metric_fn.compute(predictions=y_pred, references=y_batch, average='weighted')
+            scores.append(metric_score[metric])
     return np.sum(scores) / len(scores)
 
-def train(train_loader: DataLoader, test_loader: DataLoader, metric: str='f1', num_epochs: int=3):
+def train(train_loader: DataLoader, test_loader: DataLoader, metric: str='f1', num_epochs: int=3) -> list:
     net = ReviewNet()
     loss_fn = nn.CrossEntropyLoss()
     optimizer = optim.Adam(net.parameters(), lr=3e-4)
@@ -45,18 +45,23 @@ def train(train_loader: DataLoader, test_loader: DataLoader, metric: str='f1', n
 
     for epoch in range(num_epochs):
         print(f'\n========== epoch {epoch + 1} ==========\n')
-        for batch_idx, (X_batch, y_batch) in enumerate(tqdm(train_loader)):
+        for batch_idx, (X_batch, y_batch) in enumerate(train_loader):
+            net.train()
             optimizer.zero_grad()
             X_batch = X_batch.type(torch.float32)
             y_batch = y_batch.type(torch.int64)
             y_pred = net(X_batch)
             loss = loss_fn(y_pred, y_batch)
             loss.backward()
-            print(f'batch loss - {loss.data:.4f}')
+            print(f'Batch {batch_idx}/{len(train_loader)}, batch loss - {loss.data:.4f}')
             optimizer.step()
 
             if batch_idx % 1000 == 0:
                 f1_scores.append(get_metric_score(net, metric, test_loader))
+
+    print(f'\nModel was trained, loss - {loss.data:.4f}')
+    torch.save(net.state_dict(), './ReviewNet.model')
+    return f1_scores
 
 def main(path_to_data: str):
     data = pd.read_csv(path_to_data)
@@ -64,5 +69,9 @@ def main(path_to_data: str):
     y= data['rating']
     X_train, X_test, y_train, y_test = get_train_test_split(X, y)
     train_dataset, test_dataset = get_train_test_datasets(X_train, X_test, y_train, y_test)
-    train_loader = DataLoader(train_dataset)
+    train_loader = DataLoader(train_dataset, batch_size=)
     test_loader = DataLoader(test_dataset)
+    train(train_loader, test_loader)
+
+if __name__ == '__main__':
+    main()
